0b5bfbadd22cb9d3c1562ee282611b3ee7a3296c
pvergoulis <vergoulispaulos@gmail.com>
2025-07-02 13:53:20 +0300

    Feat: doctorLogin router and controller added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -30,3 +30,30 @@ exports.login = async(req, res)=>{
         res.status(400).json({status : false, data : error})
     }
 }
+
+
+exports.doctorLogin = async(req, res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    try {
+        const result = await Doctor.findOne({username: username}, { _id: 1,username :1, password: 1, email: 1, role:1})
+        if (!result) {
+            return res.status(404).json({ status: false, data: "Doctor-User not found" });
+        }
+
+        const isMatched =  await bcrypt.compare(password, result.password)
+
+        if(username === result.username && isMatched){
+            const token = authService.generateToken(result)
+            res.status(200).json({status:true, data: token})
+        }else{
+            res.status(404).json({status:false, data: "User not logged in"})
+        }
+
+
+    } catch (error) {
+        console.log('Error with logging', error)
+        res.status(400).json({status : false, data : error})
+    }
+}
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const authController= require('../controllers/auth.controller')
 
 router.post('/login', authController.login)
-
+router.post('/doctor-login',authController.doctorLogin)
 
 module.exports = router
